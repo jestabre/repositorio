922ad26fcc27d42fd0ac8ac719d1f179c3cdd2fc
Proyecto Sonder <sonderproyecto@gmail.com>
2023-05-03 20:05:58 -0300

    Aplicado de carrito de compras

--- canelo.js
+++ canelo.js
@@ -1,95 +1,133 @@
-/*Bienvenida al portal */
-function nombreCliente() {
-  let nombre = prompt("Bienvenido a Alimentos Canelo. Para una mejor organización de la venta de nuestros productos, indiquenos su nombre y apellido por favor");
-  alert("Le damos la bienvenida a nuestro portal, " + nombre);
-  console.log("Le damos la bienvenida a nuestro portal, " + nombre)
+const cards = document.getElementById('cards')
+const items = document.getElementById('items')
+const footer = document.getElementById('footer')
+const templateCard = document.getElementById('template-card').content
+const templateFooter = document.getElementById('template-footer').content
+const templateCarrito = document.getElementById('template-carrito').content
+const fragment = document.createDocumentFragment()
+let carrito = {}
+
+document.addEventListener('DOMContentLoaded', async e => {
+    await fetchData()
+    if (localStorage.getItem('carrito')) {
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        pintarCarrito()
+    }
+});
+
+cards.addEventListener('click', e => { addCarrito(e) });
+items.addEventListener('click', e => { btnAumentarDisminuir(e) })
+
+const fetchData = async () => {
+    const res = await fetch("/api.json");
+    const data = await res.json()
+    pintarCards(data)
 }
-for (let i = 0; i < 1; i++) {
-  nombreCliente();
+
+const pintarCards = data => {
+    data.forEach(item => {
+        templateCard.querySelector('h5').textContent = item.title
+        templateCard.querySelector('p').textContent = item.precio
+        templateCard.querySelector("img").setAttribute("src", item.thumbnailUrl)
+        templateCard.querySelector('button').dataset.id = item.id
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    cards.appendChild(fragment)
 }
 
-/* Luego de aplicar el nombre del usuario, el simulador continuará con la siguiente pregunta */
-let producto = prompt("¿Desea comprar alimento para perro o para gato?");
-
-/* declaración de las variables*/
-const NO_PRODUCTO = "";
-const PRODUCTO_P = "PERRO";
-const PRODUCTO_G = "GATO";
-
-/* Si el usuario pulsa "aceptar" sin colocar ningun dato, se aplicará este metodo" */
-while (producto == NO_PRODUCTO) {
-  alert("Debe ingresar un producto");
-  console.log("El usuario debe ingresar perro o gato");
-  producto = prompt("¿Desea comprar alimento para perro o para gato?");
+const addCarrito = e => {
+    if (e.target.classList.contains('btn-dark')) {
+        setCarrito(e.target.parentElement)
+    }
+    e.stopPropagation()
 }
-/* A partir de aquí, comienza a aplicarse el proceso de compra por el usuario" */
-if (producto.toUpperCase() == PRODUCTO_P) {
-  alert("Usted desea comprar alimento para perro");
-  console.log("El usuario desea comprar alimento para " + producto);
+
+const setCarrito = item => {
+    const producto = {
+        title: item.querySelector('h5').textContent,
+        precio: item.querySelector('p').textContent,
+        id: item.querySelector('button').dataset.id,
+        cantidad: 1
+    }
+    if (carrito.hasOwnProperty(producto.id)) {
+        producto.cantidad = carrito[producto.id].cantidad + 1
+    }
+
+    carrito[producto.id] = { ...producto }
+
+    pintarCarrito()
 }
-else if (producto.toUpperCase() == PRODUCTO_G) {
-  alert("Usted desea comprar alimento para gato");
-  console.log("El usuario desea comprar alimento para " + producto);
+
+const pintarCarrito = () => {
+    items.innerHTML = ''
+
+    Object.values(carrito).forEach(producto => {
+        templateCarrito.querySelector('th').textContent = producto.id
+        templateCarrito.querySelectorAll('td')[0].textContent = producto.title
+        templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+        templateCarrito.querySelector('span').textContent = producto.precio * producto.cantidad
+
+        //botones
+        templateCarrito.querySelector('.btn-info').dataset.id = producto.id
+        templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+
+        const clone = templateCarrito.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
+
+    pintarFooter()
+
+    localStorage.setItem('carrito', JSON.stringify(carrito))
 }
-/* Si el usuario aplica otro animal que no sea el consultado, se aplicará este metodo */
-else {
-  alert("debe ingresar perro o gato");
+
+const pintarFooter = () => {
+    footer.innerHTML = ''
+
+    if (Object.keys(carrito).length === 0) {
+        footer.innerHTML = `
+        <th scope="row" colspan="5"> <b>Carrito vacío - </b>Seleccione sus productos</th>
+        `
+        return
+    }
+
+    const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
+    const nPrecio = Object.values(carrito).reduce((acc, { cantidad, precio }) => acc + cantidad * precio, 0)
+
+    templateFooter.querySelectorAll('td')[0].textContent = nCantidad
+    templateFooter.querySelector('span').textContent = nPrecio
+
+    const clone = templateFooter.cloneNode(true)
+    fragment.appendChild(clone)
+
+    footer.appendChild(fragment)
+
+    const boton = document.querySelector('#vaciar-carrito')
+    boton.addEventListener('click', () => {
+        carrito = {}
+        pintarCarrito()
+    })
 
 }
-/* Establecemos una clase para identificar nuestros productos */
-class Marca {
-  constructor(nombre, precio) {
-    this.nombre = nombre.toUpperCase();
-    this.precio = precio;
-  }
-  toString = function () {
-    return this.nombre + " por 10 kg al precio de $" + this.precio.toFixed(1);
-  };
+
+const btnAumentarDisminuir = e => {
+    if (e.target.classList.contains('btn-info')) {
+        const producto = carrito[e.target.dataset.id]
+        producto.cantidad++
+        carrito[e.target.dataset.id] = { ...producto }
+        pintarCarrito()
+    }
+
+    if (e.target.classList.contains('btn-danger')) {
+        const producto = carrito[e.target.dataset.id]
+        producto.cantidad--
+        if (producto.cantidad === 0) {
+            delete carrito[e.target.dataset.id]
+        } else {
+            carrito[e.target.dataset.id] = { ...producto }
+        }
+        pintarCarrito()
+    }
+    e.stopPropagation()
 }
-/* Se definen la cantidad de productos que tendrá nuestro simulador */
-const misMarcas = [
-  new Marca("Purina", 4000),
-  new Marca("Royal Canin", 4750),
-  new Marca("Old Prince", 3450),
-  new Marca("Optimum", 5200),
-  new Marca("Infinity", 4500)
-];
-
-let respuestaUsuario = "";
-let suma = 0;
-let subtotal = 0;
-/* El usuario debe colocar el producto por el cual está interesado */
-do {
-  let productoAlimento = prompt(
-    "En Alimentos Canelo ofrecemos marcas tales como Purina, Royal Canin, Old Price, Optimum y Infinity, ¿en cual se encuentra interesado/a?"
-  );
-  let productoABuscar = misMarcas.find(
-    (marcasUno) => marcasUno.nombre === productoAlimento.toUpperCase()
-  );
-
-  if (productoABuscar !== undefined) {
-    /* Si el resultado es correcto, a traves de un alert se indicará el producto seleccionado y su precio */
-    alert("Ofrecemos el bolson de " + productoABuscar.toString());
-    console.log("los datos del producto seleccionado por el usuario son", productoABuscar);
-    /* Se crea la variable "subtotal", la cual nos permitirá sumar los productos que el usuario decida seleccionar para su compra */
-    subtotal += productoABuscar.precio;
-  } else {
-    /* Caso que sea incorrecto, se le indicará al usuario que coloque un producto valido */
-    alert(
-      "No encontramos ningun bolson en nuestro catalogo con el siguiente nombre: " +
-      productoAlimento +
-      ". Ingrese otro por favor"
-    ); 
-    console.log("El usuario ingresó un nombre erroneo");
-  }
-  /* Una vez que el producto se seleccione como correcto, el simulador nos indicará si queremos sumar otro producto. Si se confirma, volverá a consultar que producto está interesado */
-  respuestaUsuario = prompt(
-    "¿Desea agregar otro producto a su compra? Escriba Si o No"
-  );
-  /* Si se niega, nos dará el resultado final de los productos que seleccionó el cliente */
-} while (respuestaUsuario.toUpperCase() !== "NO");
-
-/* Se aplica la suma y el subtotal. Una vez sumado, el resultado será visualizado en la consola */
-suma += subtotal;
-
-console.log("La suma de los productos seleccionados por el usuario da un total de $", subtotal);
